7e4cd869f8405135935c5bab95bee474dd4097d5
xnacly <matteogropp@gmail.com>
2020-03-03 21:47:56 +0100

    finaly something i can look at

--- main.py
+++ main.py
@@ -7,7 +7,9 @@
 
 from json import loads as loa
 from json import dumps as dum
+from os import system as s
 import random
+
 
 def reading_module(file):
 	with open(file, "r") as f:
@@ -22,21 +24,37 @@ def format_module(content):
 		question_array.append(key)
 	lenght = len(question_array)
 	number = random.random() * lenght
-	if int(round(number)) > 1:
-		number = 1
-	final_question = question_array[int(round(number))]
+	try:
+		final_question = question_array[int(round(number))]
+	except:
+		lenght = len(question_array)
+		number = random.random() * lenght
+		final_question = question_array[int(round(number))]
 	return final_question
 
 def access_data_from_question_key(final_question, content):
+	# combines the questions with the corresponding values
 	content_json = loa(content)
-	print(final_question)
+	print("\n" + final_question + "\n")
+	answer_array = []
 	for x in content_json[final_question]:
-		print(x)
+		answer_array.append(x)
+	return answer_array
+	# answer_array[0] --> right anwser
 
 
 def main_module():
+	# combines & calls all functions
 	content = reading_module("contents.json")
 	final_question = format_module(content)
-	access_data_from_question_key(final_question, content)
+	s("cls")
+	answers = access_data_from_question_key(final_question, content)
+	for a in answers:
+		print(a)
+	usersinput = input("\n1/2/3/4? ")
+	if usersinput == "1":
+		print("\n\nRichtige Antwort :)")
+	else:
+		print("\n\nFalsche Antwort :(")
 
 main_module()
