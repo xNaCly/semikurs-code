30b92aa9f48b1e4ddd759c1dad4cc1dbbf0cc1f4
xnacly <matteogropp@gmail.com>
2020-03-05 16:57:09 +0100

    made the gui work

--- gui.py
+++ gui.py
@@ -6,24 +6,14 @@ question = main.format_questions(content)
 answer_array = main.access_data_from_question_key(question, content)
 # Answerpossibilities
 def answer_compare():
-	pass
-def a():
-    print("a")
-def b():
-    print("b")
-def c():
-    print("c")
-def d():
-    print("d")
+    print("right anwser :) " + right_answer)
 r.title("Choose or Loose")
 r.geometry("320x220")
 t.Label(r, text=question, width=250).pack()
-times = 0
+right_answer = answer_array[0]
+right_answer_button = t.Button(r, text=right_answer, width=250, command=answer_compare)
+right_answer_button.pack()
+del answer_array[0]
 for answers in answer_array:
-	times +=1
-	t.Label(r, text=str(times) + ".  " + answers, width=150).pack()
-t.Button(r, text="1", command=a, width="10").pack(side="left")
-t.Button(r, text="2", command=b, width="10").pack(side="left")
-t.Button(r, text="3", command=c, width="10").pack(side="left")
-t.Button(r, text="4", command=d, width="10").pack(side="left")
+    t.Button(r, text=answers, width=250).pack()
 r.mainloop()
