b7e3a71ce36b8e043b81a092ebb92cbc35cd88f6
xnacly <boomh0815@gmail.com>
2020-08-11 21:32:12 +0200

    final form

--- frontend/index.js
+++ frontend/index.js
@@ -3,14 +3,36 @@ const prefs = {
 	endpoints: ["/all", "/random", "/endpoints"],
 };
 
+var rightanswer = "";
+
+function submit(button) {
+	var value = button.textContent.split(" - ")[1];
+	checkForAnswer(value);
+}
+
+function checkForAnswer(answer) {
+	if (rightanswer == answer) {
+		alert("Right answer");
+		return getQuestion();
+	} else {
+		alert("Wrong answer");
+		return getQuestion();
+	}
+}
+
 async function getQuestion() {
-	let response = await fetch(prefs.base_url + prefs.endpoints[1]);
-	var { antworten, frage } = await response.json();
-	document.getElementById("question").textContent = frage;
-	document.getElementById("ant1").textContent = antworten[0];
-	document.getElementById("ant2").textContent = antworten[1];
-	document.getElementById("ant3").textContent = antworten[2];
-	document.getElementById("ant4").textContent = antworten[3];
+	try {
+		let response = await fetch(prefs.base_url + prefs.endpoints[1]);
+		var { antworten, frage, richtigeAntwort } = await response.json();
+		document.getElementById("question").textContent = frage;
+		document.getElementById("ant1").textContent = `A - ${antworten[0]}`;
+		document.getElementById("ant2").textContent = `B - ${antworten[1]}`;
+		document.getElementById("ant3").textContent = `C - ${antworten[2]}`;
+		document.getElementById("ant4").textContent = `D - ${antworten[3]}`;
+		rightanswer = richtigeAntwort;
+	} catch {
+		return alert("Request to REST-Api failed.");
+	}
 }
 
 window.addEventListener("load", async () => {
