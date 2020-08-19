c59124d8a88fd358e20ae165341f8080df1d709c
xnacly <boomh0815@gmail.com>
2020-08-19 18:22:51 +0200

    updated index.js frontend_old

--- frontend_old/index.js
+++ frontend_old/index.js
@@ -1,15 +1,18 @@
 const prefs = {
+	//change this if you got a different port, or an external api-server
 	base_url: "http://127.0.0.1:5000",
 	endpoints: ["/all", "/random", "/endpoints"],
 };
 
 var rightanswer = "";
 
+// format answer
 function submit(button) {
 	var value = button.textContent.split(" - ")[1];
 	checkForAnswer(value);
 }
 
+// self-explanatory
 function checkForAnswer(answer) {
 	if (rightanswer == answer) {
 		alert("Right answer");
@@ -22,8 +25,21 @@ function checkForAnswer(answer) {
 
 async function getQuestion() {
 	try {
+		// send request to local api server
 		let response = await fetch(prefs.base_url + prefs.endpoints[1]);
 		var { antworten, frage, richtigeAntwort } = await response.json();
+		// shuffle 'antworten' to be random
+		let temporaryValue, randomIndex;
+
+		for (let i = 0; i < antworten.length; i++) {
+			randomIndex = Math.floor(Math.random() * i);
+
+			temporaryValue = antworten[i];
+			antworten[i] = antworten[randomIndex];
+			antworten[randomIndex] = temporaryValue;
+		}
+
+		// replace placeholders with randomised 'antwort' values
 		document.getElementById("question").textContent = frage;
 		document.getElementById("ant1").textContent = `A - ${antworten[0]}`;
 		document.getElementById("ant2").textContent = `B - ${antworten[1]}`;
@@ -31,10 +47,12 @@ async function getQuestion() {
 		document.getElementById("ant4").textContent = `D - ${antworten[3]}`;
 		rightanswer = richtigeAntwort;
 	} catch {
-		return alert("Request to REST-Api failed.");
+		// if no connection is found, display the alert
+		return (document.getElementById("alert").style.display = "block");
 	}
 }
 
+// if window is loaded, try loading questions and answers
 window.addEventListener("load", async () => {
 	getQuestion();
 });
