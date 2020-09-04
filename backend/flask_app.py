27fd8b10524f13f32a2f6d37980b6273e48b5e19
xnacly <boomh0815@gmail.com>
2020-09-04 20:50:36 +0200

    :sparkles: scoreboard (backend)

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -1,5 +1,5 @@
 import backend
-from flask import Flask, jsonify, redirect, Response
+from flask import Flask, jsonify, redirect, Response, request
 import json
 app = Flask(__name__)
 data = backend.reader("./contents.json")
@@ -37,9 +37,38 @@ def all():
 	resp.headers['Access-Control-Allow-Origin'] = '*'
 	return resp, 200
 
-# @app.route("/")
-# def redi():
-    # return redirect("https://github.com/xNaCly/semikurs-code/blob/master/server/README.md", code=302)
+@app.route("/scoreboard", methods=['POST','GET'])
+def scoreboard():
+	if request.method == "POST":
+		name,score = request.args.get("name"), request.args.get("score")
+		if not name or not score:
+			return {
+				"content": {
+					"error": "missing params. syntax should be: ?name=<string>&score=<number>"
+				},
+				"status": 400
+			}
+		try:
+			with open("./scoreboard.csv", "a") as f:
+				f.write(f"\n{name},{score}")
+			return {
+				"content": {
+					"name":name,
+					"score":score
+				},
+				"status": 200
+			}
+		except:
+			return {
+				"content": {
+					"error": "failed to write score to 'scoreboard.csv'"
+				},
+				"status": 500
+			}
+	elif request.method == "GET":
+		with open("./scoreboard.csv", "r") as f:
+			return f.read(), 200
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
