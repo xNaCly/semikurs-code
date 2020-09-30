f43a21e7e6afba9a5a02d42d14d51c06224b1430
xnacly <matteogropp@gmail.com>
2020-09-30 18:53:15 +0200

    fix fix fix

--- gen_questions/gen.py
+++ gen_questions/gen.py
@@ -2,14 +2,18 @@ import json
 
 questions = {}
 
-frage_inhalt = input("Syntax => <Frage>|<richtige_Antwort>|<antwort>|<antwort>|<antwort>|<context>")
-frage_inhalt = frage_inhalt.split("|")
-questions[frage_inhalt[0]] = [
-    frage_inhalt[1],
-    frage_inhalt[2],
-    frage_inhalt[3],
-    frage_inhalt[4]
-]
-print(questions)
+with open("content.csv","r") as f:
+    content = f.read().split("\n")
+    content.pop(0)
+
+for x in content:
+    x = x.split("|")
+    questions[x[0]] = [
+        x[1],
+        x[2],
+        x[3],
+        x[4]
+    ]
+
 with open("temp.json","w") as f:
     json.dump(questions, f)
