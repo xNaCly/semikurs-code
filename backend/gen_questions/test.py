cfbe59aad85c01face5c7503e93f1bf08d2bbadf
xnacly <matteogropp@gmail.com>
2020-12-05 15:35:55 +0100

    removed test.py

--- backend/gen_questions/test.py
+++
@@ -1,9 +0,0 @@
-# from terminaltables import SingleTable
-# table_data = [["question","answers","context"]]
-# print(SingleTable(table_data).table)
-with open("content.csv","r") as f:
-    questions = f.read().split("\n")
-for item in questions:
-    print("-----") 
-    item = item.split("|")
-    print(f"\t{item[0]}\n\t{item[1:-2]}\n\t{item[-1]}")
